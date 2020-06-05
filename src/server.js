b2274124f1d25cf159fadba4afb1d4806921aed6
Santiago <ssantiagotl@gmail.com>
2020-06-05 00:17:33 -0300

    All those files were changed to simplify their configuration using nunjucks

--- src/server.js
+++ src/server.js
@@ -5,21 +5,33 @@ const server = express()
 // Public folder config
 server.use(express.static("public"))
 
+
+
+
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
 //Application path configuration
 //Home page
 //req: requisiton (asking something)
 //res: answer (returning somthing for us)
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html") //the answer we want for home page
+   return res.render("index.html", {title: "Um titulo"}) //the answer we want for home page
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html") //the answer we want for new collection point creation
+   return res.render("create-point.html") //the answer we want for new collection point creation
 })
 
 server.get("/search-results", (req, res) => {
-    res.sendFile(__dirname + "/views/search-results.html") //the answer we want for new collection point creation
+   return res.render("search-results.html") //the answer we want for new collection point creation
 })
 
 
