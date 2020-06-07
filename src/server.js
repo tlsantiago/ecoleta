facce13310258ecc0f87b2e9768e420638689879
Santiago <ssantiagotl@gmail.com>
2020-06-07 02:12:13 -0300

    Added DB import to server

--- src/server.js
+++ src/server.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const server = express()
 
+// DB sync
+const db = require("./database/db.js")
 
 // Public folder config
 server.use(express.static("public"))
 
+ // Req.body enabled
+server.use(express.urlencoded({ extended: true }))
 
 
 
-
-// utilizando template engine
+// Using template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
     express: server,
@@ -27,11 +30,71 @@ server.get("/", (req, res) => {
 })
 
 server.get("/create-point", (req, res) => {
+
+
    return res.render("create-point.html") //the answer we want for new collection point creation
 })
 
+server.post("/create-point", (req, res) => {
+
+   // Insert data into DB using req.body
+   const query = `
+   INSERT INTO places (
+       image,
+       name,
+       address,
+       address2,
+       state,
+       city,
+       items
+   ) VALUES (?, ?, ?, ?, ?, ?, ?);
+`
+
+const values = [
+   req.body.image,
+   req.body.name,
+   req.body.address,
+   req.body.address2,
+   req.body.state,
+   req.body.city,
+   req.body.items
+]
+
+function afterInsertData(err) {
+   if(err) {
+       return res.send("Erro no cadastro!")
+   }
+
+   console.log("Successfully registered")
+   console.log(this)
+
+   return res.render("create-point.html", {saved: true})
+}
+
+db.run(query, values, afterInsertData)
+
+})
+
+
 server.get("/search-results", (req, res) => {
-   return res.render("search-results.html") //the answer we want for new collection point creation
+
+   const search = req.query.search
+
+   if(search == "") {
+      return res.render("search-results.html", { total: 0 })
+   }
+
+   // get data from database
+   db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+      if(err) {
+          return console.log(err);
+      }
+
+      const total = rows.length
+
+      // Showing DB data on html
+      return res.render("search-results.html", { places: rows, total: total })
+  })
 })
 
 
